42dcf1d2b13bfadf53e8a953d3bbf040d98bcf16
IAmCodeGeek <copewithcode@gmail.com>
2025-04-09 14:03:09 +0530

    Change

--- app.py
+++ app.py
@@ -6,28 +6,7 @@ app = Flask(__name__)
 # Routes
 @app.route('/')
 def index():
-    pass
-
-@app.route('/predict-batch', methods=['GET', 'POST'])
-def predict_batch():
-    pass
-
-@app.route('/predict-single', methods=['GET', 'POST'])
-def predict_single():
-    pass
-
-@app.route('/api/predict', methods=['POST'])
-def api_predict():
-    pass
-
-@app.route('/model-info')
-def model_info():
-    pass
-
-# Add example data upload
-@app.route('/download-template')
-def download_template():
-    pass
+    return jsonify({"message": "Your deployment is successful"})
 
 if __name__ == '__main__':
     app.run(debug=True)
